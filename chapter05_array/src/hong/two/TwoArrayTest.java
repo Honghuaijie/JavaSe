44f0199e4852d4719b2b9b0279e4546a66edccf6
Honghuaijie <2522030954@qq.com>
2023-07-25 09:52:09 +0800

    0725

---
+++ chapter05_array/src/hong/two/TwoArrayTest.java
@@ -0,0 +1,26 @@
+package hong.two;
+
+/**
+ * ClassName: TwoArrayTest
+ * Package: hong.two
+ * Description:
+ *
+ * @Author honghuaijie
+ * @Create 2023/7/25 9:46
+ * @Version 1.0
+ * 不积跬步无以至千里
+ */
+public class TwoArrayTest {
+    public static void main(String[] args) {
+//                数组的声明和初始化
+
+//                调用数组的指定元素
+
+
+//                数组的长度：length
+//
+//                数组的遍历
+
+
+    }
+}
---
+++ chapter05_array/src/hong/two/TwoArrayTest2.java
@@ -0,0 +1,19 @@
+package hong.two;
+
+/**
+ * ClassName: TwoArrayTest2
+ * Package: hong.two
+ * Description:
+ *
+ * @Author honghuaijie
+ * @Create 2023/7/25 9:50
+ * @Version 1.0
+ * 不积跬步无以至千里
+ */
+public class TwoArrayTest2 {
+    public static void main(String[] args) {
+//          数组元素的默认初始化值
+
+//          一维数组的内存解析
+    }
+}
